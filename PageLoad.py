4c642da258b839ab84be5094b8e6857d69ab3887
Nikita Gokavi <nikita.gokavi@travelcircus.local>
2026-09-12 12:58:39 +0200

    First commit: existing PageLoading project files

---
+++ PageLoad.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import requests
+import json
+import os
+from binascii import a2b_base64
+from pathlib import Path
+from urllib.parse import urlparse
+from urllib import request
+import pandas as pd
+import traceback
+from datetime import date
+import mysql.connector
+from mysql.connector import Error
+
+with open('config.json') as json_file:
+    dbConfig = json.load(json_file)
+
+strategies = ['desktop', 'mobile']
+apikey = dbConfig['apikey']
+today = str(date.today())
+
+from datetime import datetime
+
+
+# In[2]:
+
+
+def savetoFile(lhtest, summary, url, timestampStr):
+    urlelements = urlparse(url)
+    domainp = urlelements.netloc
+    path = urlelements.path
+    if not path.endswith('/'):
+        path = path + '/'
+    basepath = os.getcwd() + "/testresults/" + domainp + path
+    filename = basepath + f'{timestampStr}.json'
+    result = Path(basepath +f'{timestampStr}.json')
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    timestampfilename = timestampStr.replace(":", "-")
+    with open(basepath + f'{timestampfilename}-{strategy}.json', 'w+') as re:
+        imptMat = {"loadingExperience":lhtest["loadingExperience"],
+                   "originLoadingExperience":lhtest["originLoadingExperience"],
+                   "summary" : summary};
+        jsonstr = json.dumps(imptMat, ensure_ascii=False, indent=4)
+        re.write(jsonstr)
+        re.close()
+        print(basepath + "results saved")
+
+
+# In[3]:
+
+
+def savetoDB(summary):
+    try:
+        connection = mysql.connector.connect(host=dbConfig['host'],
+                                             database=dbConfig['database'],
+                                             user=dbConfig['user'],
+                                             port=dbConfig['port'],
+                                             password=dbConfig['password'])
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            cursor.execute("""INSERT into pagespeed (url, env, device, timestamp, overallperformancescore, fcp, fid, lcp, cls, fcp_score, fid_score, lcp_score, cls_score, inp, ttfb, tbt, si) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                               (summary['url'],
+                                summary['env'],
+                                summary['strategy'],
+                                summary['timestamp'],
+                                summary['OverallPerformanceScore'],
+                                summary['fcp'],
+                                summary['fid'],
+                                summary['lcp'],
+                                summary['cls'],
+                                summary['fcp_score'],
+                                summary['fid_score'],
+                                summary['lcp_score'],
+                                summary['cls_score'],
+                                summary['inp'],
+                                summary['ttfb'],
+                                summary['tbt'],
+                                summary['si']
+                               ))
+            connection.commit()
+            print("data saved into table")
+
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("sql connection is closed")
+
+
+# In[5]:
+
+
+with open('urllist.json') as json_file:
+    data = json.load(json_file)
+    for strategy in strategies:
+        for key, value in data.items():
+            for url in value:
+                f = '%Y-%m-%d %H:%M:%S'
+                dateTimeObj = datetime.now()
+                timestampStr = dateTimeObj.strftime(f)
+                print("\nStart Lighthouse-Test for " + url)
+                lhscores = []
+                print("\nFetching results from api", url)
+                x = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={url}&strategy={strategy}{apikey}'
+                response = requests.get(x)
+                lhtest = response.json()
+                loadingExperience = lhtest["loadingExperience"]["metrics"]
+                audits = lhtest["lighthouseResult"]["audits"]
+                
+                if lhtest.get('id',False):
+                    summary = {
+                        "url": lhtest["id"],
+                        "OverallPerformanceScore":lhtest["lighthouseResult"]["categories"]["performance"]["score"] * 100,
+                        "fcp": loadingExperience["FIRST_CONTENTFUL_PAINT_MS"]["percentile"] if "FIRST_CONTENTFUL_PAINT_MS" in loadingExperience else None,
+                        "fid": loadingExperience["FIRST_INPUT_DELAY_MS"]["percentile"] if "FIRST_INPUT_DELAY_MS" in loadingExperience else None,
+                        "lcp": loadingExperience["LARGEST_CONTENTFUL_PAINT_MS"]["percentile"] if "LARGEST_CONTENTFUL_PAINT_MS" in loadingExperience else None,
+                        "cls": loadingExperience["CUMULATIVE_LAYOUT_SHIFT_SCORE"]["percentile"]/100 if "CUMULATIVE_LAYOUT_SHIFT_SCORE" in loadingExperience else None,
+                        "fcp_score": loadingExperience["FIRST_CONTENTFUL_PAINT_MS"]["category"] if "FIRST_CONTENTFUL_PAINT_MS" in loadingExperience else None,
+                        "fid_score": loadingExperience["FIRST_INPUT_DELAY_MS"]["category"] if "FIRST_INPUT_DELAY_MS" in loadingExperience else None,
+                        "lcp_score": loadingExperience["LARGEST_CONTENTFUL_PAINT_MS"]["category"] if "LARGEST_CONTENTFUL_PAINT_MS" in loadingExperience else None,
+                        "cls_score": loadingExperience["CUMULATIVE_LAYOUT_SHIFT_SCORE"]["category"] if "CUMULATIVE_LAYOUT_SHIFT_SCORE" in loadingExperience else None,
+                        "inp": loadingExperience["EXPERIMENTAL_INTERACTION_TO_NEXT_PAINT"]["percentile"] if "EXPERIMENTAL_INTERACTION_TO_NEXT_PAINT" in loadingExperience else None,
+                        "ttfb": loadingExperience["EXPERIMENTAL_TIME_TO_FIRST_BYTE"]["percentile"]/100 if "EXPERIMENTAL_TIME_TO_FIRST_BYTE" in loadingExperience else None,
+                        "tbt": audits["total-blocking-time"]["score"] if "total-blocking-time" in audits else None,
+                        "si": audits["speed-index"]["score"] if "speed-index" in audits else None,
+                        "timestamp": timestampStr,
+                        "strategy": strategy,
+                        "env": key
+                    }
+                savetoDB(summary)
+                savetoFile(lhtest, summary, url, timestampStr)
+
+
+# In[ ]:
---
+++ README.md
@@ -0,0 +1,74 @@
+# Pageloading project
+
+## _Page Speed Insights Test_
+
+PSI is a script that is written in Python3 which uses Google service of measuring the loading speed of your webpage. 
+Currently, it is configured to run for both mobile and desktop and will create different metrics for each. 
+Capturing the real-time page scores (current setup is every four hours - configurable) helps you to analyze the comparison with other pages and the page itself. 
+There are variety of measures PageSpeed API returns, but we are currently interested in core web vitals: 
+
+- Overall Performance Score,
+- "fcp": FIRST_CONTENTFUL_PAINT_MS,
+- "fid": FIRST_INPUT_DELAY_MS,
+- "lcp": LARGEST_CONTENTFUL_PAINT_MS,
+- "cls": CUMULATIVE_LAYOUT_SHIFT_SCORE,
+- “fcp_score”: FIRST_CONTENTFUL_PAINT_MS,
+- "fid_score": FIRST_INPUT_DELAY_MS,
+- “lcp_score”:LARGEST_CONTENTFUL_PAINT_MS,
+- "cls_score": CUMULATIVE_LAYOUT_SHIFT_SCORE,
+- "inp": EXPERIMENTAL_INTERACTION_TO_NEXT_PAINT,
+- "ttfb": EXPERIMENTAL_TIME_TO_FIRST_BYTE
+- "tbt": total-blocking-time
+- "si": speed-index
+
+# Input of the script: 
+
+Urllist.json contains list of urls under test.
+
+# Output of the script:
+
+The script saves the core web vitals directly into the database 
+ 
+- Link: BI-02.tcsrv.net:3306
+- Database: QA 
+- Table: pagespeed 
+
+Every row of the table consists of core web vitals along with time stamps, devices (mobile or desktop) and env, and other few things. The table is connected with the visualization tool as the script run every four hours and saves the result into the database so the visualization tool shows the cumulative result for comparison. 
+
+There is another outfile of this script which is a json file for every web page. This is will created in the same directory with same environment all the webpages will be saved in the environment folder. In this file, you can find detail information from the webpage along with core web vitals. 
+
+# How to run (locally):
+
+A configuration file that includes the below-listed variables.
+
+```
+{
+    "host": "",
+    "database": "",
+    "user": "",
+    "port" : "",
+    "password": "",
+    "apikey" : "&key="
+}
+```
+
+This file will not be found in the repo you may need to ask someone from QA or DevOps team. 
+
+Secondly, on your local machine, you have running connection with MariaDB through DBeaver, for example and you should created a SSH tunnel like this
+
+```
+ssh -fN -L 3307:127.0.0.1:3306 username@bi-02.tcsrv.net 
+```
+
+Please get your credentials from DevOps team
+
+# Dependencies 
+
+- Python 3 (latest version)
+- juypter notebook (for editing) 
+- DBviewer (please ask DevOps team (Mercy or Adrian) for credentials and other things to connect BI-02)
+- SSH key (please ask DevOps team or Adrian)
+
+# CI/CD setup:
+
+TODO
